64081ebbeec4bb4017b0c2e462b016cb5dd98a5b
Alex Caudill <alex.caudill@gmail.com>
2012-09-11 04:55:55 +0000

    restore illumos gelf.h

--- gelf.h
+++ gelf.h
@@ -1,108 +1,147 @@
-/*-
- * Copyright (c) 2006,2008 Joseph Koshy
- * All rights reserved.
+/*
+ * CDDL HEADER START
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
  *
- * $Id: gelf.h 1168 2010-09-04 01:03:25Z jkoshy $
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+/*
+ * Copyright 2004 Sun Microsystems, Inc.  All rights reserved.
+ * Use is subject to license terms.
  */
 
-#ifndef	_GELF_H_
-#define	_GELF_H_
+#ifndef	_GELF_H
+#define	_GELF_H
 
-#include <sys/cdefs.h>
+#pragma ident	"%Z%%M%	%I%	%E% SMI"
+
+#include <sys/feature_tests.h>
 
 #include <libelf.h>
+#include <sys/link.h>
 
-typedef Elf64_Addr	GElf_Addr;	/* Addresses */
-typedef Elf64_Half	GElf_Half;	/* Half words (16 bit) */
-typedef Elf64_Off	GElf_Off;	/* Offsets */
-typedef Elf64_Sword	GElf_Sword;	/* Signed words (32 bit) */
-typedef Elf64_Sxword	GElf_Sxword;	/* Signed long words (64 bit) */
-typedef Elf64_Word	GElf_Word;	/* Unsigned words (32 bit) */
-typedef Elf64_Xword	GElf_Xword;	/* Unsigned long words (64 bit) */
+#ifdef	__cplusplus
+extern "C" {
+#endif
 
-typedef Elf64_Dyn	GElf_Dyn;	/* ".dynamic" section entries */
-typedef Elf64_Ehdr	GElf_Ehdr;	/* ELF header */
-typedef Elf64_Phdr	GElf_Phdr;	/* Program header */
-typedef Elf64_Shdr	GElf_Shdr;	/* Section header */
-typedef Elf64_Sym	GElf_Sym;	/* Symbol table entries */
-typedef Elf64_Rel	GElf_Rel;	/* Relocation entries */
-typedef Elf64_Rela	GElf_Rela;	/* Relocation entries with addend */
 
-typedef	Elf64_Cap	GElf_Cap;	/* SW/HW capabilities */
-typedef Elf64_Move	GElf_Move;	/* Move entries */
-typedef Elf64_Syminfo	GElf_Syminfo;	/* Symbol information */
+#if !defined(_INT64_TYPE)
+#error "64-bit integer types are required by gelf."
+#endif
 
-#define	GELF_M_INFO			ELF64_M_INFO
-#define	GELF_M_SIZE			ELF64_M_SIZE
-#define	GELF_M_SYM			ELF64_M_SYM
+/*
+ * Class-independent ELF API for Elf utilities.  This is
+ * for manipulating Elf32 and Elf64 specific information
+ * in a format common to both classes.
+ */
 
-#define	GELF_R_INFO			ELF64_R_INFO
-#define	GELF_R_SYM			ELF64_R_SYM
-#define	GELF_R_TYPE			ELF64_R_TYPE
-#define	GELF_R_TYPE_DATA		ELF64_R_TYPE_DATA
-#define	GELF_R_TYPE_ID			ELF64_R_TYPE_ID
-#define	GELF_R_TYPE_INFO		ELF64_R_TYPE_INFO
+typedef Elf64_Addr	GElf_Addr;
+typedef Elf64_Half	GElf_Half;
+typedef Elf64_Off	GElf_Off;
+typedef Elf64_Sword	GElf_Sword;
+typedef Elf64_Sxword	GElf_Sxword;
+typedef Elf64_Word	GElf_Word;
+typedef Elf64_Xword	GElf_Xword;
 
-#define	GELF_ST_BIND			ELF64_ST_BIND
-#define	GELF_ST_INFO			ELF64_ST_INFO
-#define	GELF_ST_TYPE			ELF64_ST_TYPE
-#define	GELF_ST_VISIBILITY		ELF64_ST_VISIBILITY
+typedef Elf64_Ehdr	GElf_Ehdr;
+typedef	Elf64_Move	GElf_Move;
+typedef Elf64_Phdr	GElf_Phdr;
+typedef Elf64_Shdr	GElf_Shdr;
+typedef Elf64_Sym	GElf_Sym;
+typedef	Elf64_Syminfo	GElf_Syminfo;
+typedef Elf64_Rela	GElf_Rela;
+typedef Elf64_Rel	GElf_Rel;
+typedef Elf64_Dyn	GElf_Dyn;
+typedef Elf64_Cap	GElf_Cap;
 
-__BEGIN_DECLS
-long		gelf_checksum(Elf *_elf);
-size_t		gelf_fsize(Elf *_elf, Elf_Type _type, size_t _count,
-			unsigned int _version);
-int		gelf_getclass(Elf *_elf);
-GElf_Dyn	*gelf_getdyn(Elf_Data *_data, int _index, GElf_Dyn *_dst);
-GElf_Ehdr	*gelf_getehdr(Elf *_elf, GElf_Ehdr *_dst);
-GElf_Phdr	*gelf_getphdr(Elf *_elf, int _index, GElf_Phdr *_dst);
-GElf_Rel	*gelf_getrel(Elf_Data *_src, int _index, GElf_Rel *_dst);
-GElf_Rela	*gelf_getrela(Elf_Data *_src, int _index, GElf_Rela *_dst);
-GElf_Shdr	*gelf_getshdr(Elf_Scn *_scn, GElf_Shdr *_dst);
-GElf_Sym	*gelf_getsym(Elf_Data *_src, int _index, GElf_Sym *_dst);
-GElf_Sym	*gelf_getsymshndx(Elf_Data *_src, Elf_Data *_shindexsrc,
-			int _index, GElf_Sym *_dst, Elf32_Word *_shindexdst);
-void *		gelf_newehdr(Elf *_elf, int _class);
-void *		gelf_newphdr(Elf *_elf, size_t _phnum);
-int		gelf_update_dyn(Elf_Data *_dst, int _index, GElf_Dyn *_src);
-int		gelf_update_ehdr(Elf *_elf, GElf_Ehdr *_src);
-int		gelf_update_phdr(Elf *_elf, int _index, GElf_Phdr *_src);
-int		gelf_update_rel(Elf_Data *_dst, int _index, GElf_Rel *_src);
-int		gelf_update_rela(Elf_Data *_dst, int _index, GElf_Rela *_src);
-int		gelf_update_shdr(Elf_Scn *_dst, GElf_Shdr *_src);
-int		gelf_update_sym(Elf_Data *_dst, int _index, GElf_Sym *_src);
-int		gelf_update_symshndx(Elf_Data *_symdst, Elf_Data *_shindexdst,
-			int _index, GElf_Sym *_symsrc, Elf32_Word _shindexsrc);
-Elf_Data 	*gelf_xlatetof(Elf *_elf, Elf_Data *_dst, const Elf_Data *_src, unsigned int _encode);
-Elf_Data 	*gelf_xlatetom(Elf *_elf, Elf_Data *_dst, const Elf_Data *_src, unsigned int _encode);
+/*
+ * The processing of versioning information can stay the
+ * same because both the Elf32 and Elf64 structures are
+ * of equal sizes.
+ */
+typedef Elf64_Verdef	GElf_Verdef;
+typedef Elf64_Verdaux	GElf_Verdaux;
+typedef Elf64_Verneed	GElf_Verneed;
+typedef Elf64_Vernaux	GElf_Vernaux;
+typedef Elf64_Versym	GElf_Versym;
 
-GElf_Cap	*gelf_getcap(Elf_Data *_data, int _index, GElf_Cap *_cap);
-GElf_Move	*gelf_getmove(Elf_Data *_src, int _index, GElf_Move *_dst);
-GElf_Syminfo	*gelf_getsyminfo(Elf_Data *_src, int _index, GElf_Syminfo *_dst);
-int		gelf_update_cap(Elf_Data *_dst, int _index, GElf_Cap *_src);
-int		gelf_update_move(Elf_Data *_dst, int _index, GElf_Move *_src);
-int		gelf_update_syminfo(Elf_Data *_dst, int _index, GElf_Syminfo *_src);
-__END_DECLS
+/*
+ * move.m_info is encoded using the 64bit fields in Gelf.
+ */
+#define	GELF_M_SYM	ELF64_M_SYM
+#define	GELF_M_SIZE	ELF64_M_SIZE
 
-#endif	/* _GELF_H_ */
+/*
+ * sym.st_info field is same size for Elf32 and Elf64.
+ */
+#define	GELF_ST_BIND	ELF64_ST_BIND
+#define	GELF_ST_TYPE	ELF64_ST_TYPE
+#define	GELF_ST_INFO	ELF64_ST_INFO
+
+
+/*
+ * Elf64 r_info may have data field in type id's word,
+ * so GELF_R_TYPE is defined as ELF64_R_TYPE_ID in order
+ * to isolate the proper bits for the true type id.
+ */
+#define	GELF_R_TYPE		ELF64_R_TYPE_ID
+#define	GELF_R_SYM		ELF64_R_SYM
+#define	GELF_R_INFO		ELF64_R_INFO
+#define	GELF_R_TYPE_DATA	ELF64_R_TYPE_DATA
+#define	GELF_R_TYPE_ID		ELF64_R_TYPE_ID
+#define	GELF_R_TYPE_INFO	ELF64_R_TYPE_INFO
+
+
+
+int		gelf_getclass(Elf*);
+size_t		gelf_fsize(Elf *, Elf_Type, size_t, uint_t);
+GElf_Ehdr *	gelf_getehdr(Elf *, GElf_Ehdr *);
+int		gelf_update_ehdr(Elf *, GElf_Ehdr *);
+unsigned long	gelf_newehdr(Elf *, int);
+GElf_Phdr *	gelf_getphdr(Elf *, int, GElf_Phdr *);
+int		gelf_update_phdr(Elf *, int, GElf_Phdr *);
+unsigned long	gelf_newphdr(Elf *, size_t);
+GElf_Shdr *	gelf_getshdr(Elf_Scn *,  GElf_Shdr *);
+int		gelf_update_shdr(Elf_Scn *, GElf_Shdr *);
+Elf_Data *	gelf_xlatetof(Elf *, Elf_Data *, const Elf_Data *, uint_t);
+Elf_Data *	gelf_xlatetom(Elf *, Elf_Data *, const Elf_Data *, uint_t);
+GElf_Sym *	gelf_getsym(Elf_Data *, int, GElf_Sym *);
+int		gelf_update_sym(Elf_Data *, int, GElf_Sym *);
+GElf_Sym *	gelf_getsymshndx(Elf_Data *, Elf_Data *, int, GElf_Sym *,
+		    Elf32_Word *);
+int		gelf_update_symshndx(Elf_Data *, Elf_Data *, int, GElf_Sym *,
+		    Elf32_Word);
+GElf_Syminfo *	gelf_getsyminfo(Elf_Data *, int, GElf_Syminfo *);
+int		gelf_update_syminfo(Elf_Data *, int, GElf_Syminfo *);
+GElf_Move *	gelf_getmove(Elf_Data *, int, GElf_Move *);
+int		gelf_update_move(Elf_Data *, int, GElf_Move *);
+GElf_Dyn *	gelf_getdyn(Elf_Data *, int, GElf_Dyn *);
+int		gelf_update_dyn(Elf_Data *, int, GElf_Dyn *);
+GElf_Rela *	gelf_getrela(Elf_Data *, int, GElf_Rela *);
+int		gelf_update_rela(Elf_Data *, int, GElf_Rela *);
+GElf_Rel *	gelf_getrel(Elf_Data *, int, GElf_Rel *);
+int		gelf_update_rel(Elf_Data *, int, GElf_Rel *);
+long		gelf_checksum(Elf *);
+GElf_Cap *	gelf_getcap(Elf_Data *, int, GElf_Cap *);
+int		gelf_update_cap(Elf_Data *, int, GElf_Cap *);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* _GELF_H */
